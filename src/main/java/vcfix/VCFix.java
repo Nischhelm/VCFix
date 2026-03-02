67a491c61b6ac446e51ddd0529ecbe3ad1e43325
nischhelm <nischhelm@gmail.com>
2026-03-02 13:41:52 +0100

    v1.0.3.2
    appease startup logs even more...
    man this shit is so stupid

--- src/main/java/vcfix/VCFix.java
+++ src/main/java/vcfix/VCFix.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class VCFix {
     public static final String MODID = "vcfix";
-    public static final String VERSION = "1.0.3.1";
+    public static final String VERSION = "1.0.3.2";
     public static final String NAME = "VCFix";
     public static final Logger LOGGER = LogManager.getLogger();
 }
